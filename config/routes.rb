28d50a3e1bd745e1868267de6ec39f8674ec197a
Christopher de Alba <superototus@gmail.com>
2014-02-15 23:39:25 -0800

    Works, now signing in for first time as in video 'GtUM'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Network::Application.routes.draw do
+  devise_for :users
   get "landing_page/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
