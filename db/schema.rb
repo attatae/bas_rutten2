d5d1ed19cba7843ccd3fbadb135c89bf2e3fbea1
Christopher de Alba <superototus@gmail.com>
2014-02-16 00:54:10 -0800

    Finish 'MigStat.' Comment complication but is expected, and also still anticipating event submission error up attr changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216072805) do
+ActiveRecord::Schema.define(version: 20140216081744) do
 
   create_table "comments", force: true do |t|
-    t.string   "commenter"
     t.text     "body"
     t.integer  "event_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["event_id"], name: "index_comments_on_event_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "events", force: true do |t|
     t.string   "title"
